eb3b161a6ca67bdf37775796e19211cd50ef29ff
jakaria98 <jhossain439@gmail.com>
2024-04-03 02:04:33 -0230

    controller for student

---
+++ src/main/java/com/libraryManagement/LibraryManagement/Controller/StudentController.java
@@ -0,0 +1,47 @@
+package com.libraryManagement.LibraryManagement.Controller;
+
+import com.libraryManagement.LibraryManagement.Model.Book;
+import com.libraryManagement.LibraryManagement.Model.Student;
+import com.libraryManagement.LibraryManagement.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        Student createdStudent = studentService.createStudent(student);
+        return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{studentId}")
+    public ResponseEntity<Student> getStudentById(@PathVariable String studentId) {
+        Student student = studentService.getStudentById(studentId);
+        if (student != null) {
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/{studentId}/borrow")
+    public ResponseEntity<Void> borrowBook(@PathVariable String studentId, @RequestBody Book book) {
+        studentService.borrowBook(studentId, book);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/{studentId}/return")
+    public ResponseEntity<Void> returnBook(@PathVariable String studentId, @RequestBody Book book) {
+        studentService.returnBook(studentId, book);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
