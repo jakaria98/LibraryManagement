246759484ff40c0edfb4fb2cc144e4a1bff04faf
jakaria98 <jhossain439@gmail.com>
2024-04-03 01:50:42 -0230

    Librarian service impl done

--- src/main/java/com/libraryManagement/LibraryManagement/Service/LibrarianServiceImpl.java
+++ src/main/java/com/libraryManagement/LibraryManagement/Service/LibrarianServiceImpl.java
@@ -1,29 +1,43 @@
 package com.libraryManagement.LibraryManagement.Service;
 
 import com.libraryManagement.LibraryManagement.Model.Librarian;
+import com.libraryManagement.LibraryManagement.Repo.LibrarianRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LibrarianServiceImpl implements LibrarianService {
+
+    @Autowired
+    private LibrarianRepository librarianRepository;
+
     @Override
     public Librarian createLibrarian(Librarian librarian) {
-        // Implement logic to create librarian
-        return null;
+        return librarianRepository.save(librarian);
     }
 
     @Override
     public Librarian getLibrarianById(String librarianId) {
-        // Implement logic to get librarian by ID
-        return null;
+        Optional<Librarian> optionalLibrarian = librarianRepository.findById(librarianId);
+        return optionalLibrarian.orElse(null);
     }
 
     @Override
     public void updateLibrarian(String librarianId, Librarian updatedLibrarian) {
-        // Implement logic to update librarian
+        Optional<Librarian> optionalLibrarian = librarianRepository.findById(librarianId);
+        if (optionalLibrarian.isPresent()) {
+            Librarian existingLibrarian = optionalLibrarian.get();
+            existingLibrarian.setEmployeeNumber(updatedLibrarian.getEmployeeNumber());
+            existingLibrarian.setName(updatedLibrarian.getName());
+            // Set other attributes as needed
+            librarianRepository.save(existingLibrarian);
+        }
     }
 
     @Override
     public void deleteLibrarian(String librarianId) {
-        // Implement logic to delete librarian
+        librarianRepository.deleteById(librarianId);
     }
 }
